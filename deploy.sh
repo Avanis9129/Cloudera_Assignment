603f19a8b23b1529abac8e4a7f30bb27582da74e
Avanis9129 <83877524+Avanis9129@users.noreply.github.com>
2023-11-21 16:09:37 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,3 @@
-#!/bin/bash
-source variable.sh
-
-#configure aws and cdp
-aws configure set default.region ${aws_region}
-aws configure set aws_access_key_id ${aws_access_key}
-aws configure set aws_secret_access_key ${aws_secret_key}
-
-cdp configure set default.region ${cdp_region}
-cdp configure set cdp_access_key_id ${cdp_access_key}
-cdp configure set cdp_private_key ${cdp_secret_key}
-
-#deploy cdp
-terraform apply --auto-approve
-
-#deploy cml service
-python -m venv ~/cdp-navigator; source ~/cdp-navigator/bin/activate;
-pip install ansible-core ansible-navigator
-
-rm -rf cloudera-deploy/
-git clone https://github.com/cloudera-labs/cloudera-deploy.git; cd cloudera-deploy/public-cloud/aws/cml
-
-
-#export AWS_PROFILE=default
-#export CDP_PROFILE=default
-
-ansible-navigator run main.yml
+terraform init
+terraform plan
+#terraform apply --auto-approve
